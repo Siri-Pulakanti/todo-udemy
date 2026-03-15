fbd7c056ccce97c34b53cb516d69f53a967ebcf0
Siri-Pulakanti <pulakantisiri1@gmail.com>
2026-03-15 19:11:28 +0530

    Addition,Deletion

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const date = new Date();
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright {date.getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,10 +1,17 @@
 import DeleteIcon from "@mui/icons-material/Delete";
 
 function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
   return (
     <div className="note">
-      Note.
-      <DeleteIcon />
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>
+        <DeleteIcon />
+      </button>
     </div>
   );
 }
